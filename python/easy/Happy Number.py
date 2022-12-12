a387cf34b46db8a4e1f74a9d5d56fe2b1564719a
Stepanov-hub <61012809+Stepanov-hub@users.noreply.github.com>
2022-12-12 23:29:41 +0300

    hash

---
+++ python/easy/Happy Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        while n!=1 and n!=4:
+            sum = 0
+            while n > 0:
+                sum += (n%10)**2
+                n = int(n/10)
+            n = sum
+
+        return n == 1
+
---
+++ python/easy/Roman to Integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        dict_consts = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000, 'IV':4, 'IX':9, 'XL':40, 'XC':90,
+                       'CD':400, 'CM':900}
+
+        it = 0
+        len_s = len(s)
+        num = 0
+
+        while it+1 < len_s:
+            if s[it:it+2] in dict_consts:
+                num += dict_consts[s[it:it+2]]
+                it += 2
+            else:
+                num += dict_consts[s[it]]
+                it += 1
+
+        if it < len_s:
+            num += dict_consts[s[it]]
+
+        return num
